53d01ff445c054ed7b83996b989faae1aaca5d22
Lopes Oliveira <mola@gft.com>
2020-07-09 09:20:45 -0300

    - new producer

--- src/main/java/info/marceloweb/kafkaexample/NewOrderMain.java
+++ src/main/java/info/marceloweb/kafkaexample/NewOrderMain.java
@@ -1,5 +1,6 @@
 package info.marceloweb.kafkaexample;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -14,14 +15,18 @@ public class NewOrderMain {
         var producer = new KafkaProducer<String, String>(properties());
         var value = "123, 1234,12345,8889,2355";
         var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
-        producer.send(record, (data, ex) -> {
-            if(ex != null) {
+        Callback callback = (data, ex) -> {
+            if (ex != null) {
                 ex.printStackTrace();
                 return;
             }
             System.out.println("sucesso enviando " + data.topic() + ":::partition " + data.partition()
                     + "/ offset " + data.offset() + data.timestamp());
-        }).get();
+        };
+        var email = "Thank you for your order! We are processing your order!";
+        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
+        producer.send(record, callback).get();
+        producer.send(emailRecord).get();
     }
 
     private static Properties properties() {
