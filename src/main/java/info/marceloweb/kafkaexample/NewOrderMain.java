9662d154ae83a46c640d38669e59f14e86cf2e2f
Marcelo Oliveira <marcelo@marceloweb.info>
2020-10-26 20:25:04 -0300

    - refactoring: string to serializer

--- src/main/java/info/marceloweb/kafkaexample/NewOrderMain.java
+++ src/main/java/info/marceloweb/kafkaexample/NewOrderMain.java
@@ -1,20 +1,26 @@
 package info.marceloweb.kafkaexample;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        try ( var dispatcher = new KafkaDispatcher()) {
-            for (var i = 0; i < 10; i++) {
+        try ( var orderDispatcher = new KafkaDispatcher<Order>()) {
+            try ( var emailDispatcher = new KafkaDispatcher<String>()) {
+                for (var i = 0; i < 10; i++) {
 
-                var key = UUID.randomUUID().toString();
-                var value = key + "123, 1234,12345,8889,2355";
-                dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
+                    var userId = UUID.randomUUID().toString();
+                    var orderId = UUID.randomUUID().toString();
+                    var amount = new BigDecimal(Math.random() * 5000 + 1);
+                    var order = new Order(userId, orderId, amount);
+                    orderDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
 
-                var email = "Thank you for your order! We are processing your order!";
-                dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
+                    var email = "Thank you for your order! We are processing your order!";
+                    emailDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, email);
+                }
+
             }
         }
     }
