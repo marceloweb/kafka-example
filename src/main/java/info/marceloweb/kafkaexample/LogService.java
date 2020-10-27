df04ab0b99ae79686e8016386f64e4cce5fe9b42
Marcelo Oliveira <marcelo@marceloweb.info>
2020-10-27 16:29:00 -0300

    - logs

--- src/main/java/info/marceloweb/kafkaexample/LogService.java
+++ src/main/java/info/marceloweb/kafkaexample/LogService.java
@@ -2,30 +2,27 @@ package info.marceloweb.kafkaexample;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
-public class EmailService {
+import java.util.regex.Pattern;
+
+public class LogService {
 
     public static void main(String[] args) {
-        var emailService = new EmailService();
-        try (var service = new KafkaService(EmailService.class.getSimpleName(),
-                "ECOMMERCE_SEND_EMAIL",
-                emailService::parse)) {
+        var logService = new LogService();
+        try (var service = new KafkaService(LogService.class.getSimpleName(),
+                Pattern.compile("ECOMMERCE.*"),
+                logService::parse)) {
             service.run();
         }
+
     }
 
     private void parse(ConsumerRecord<String, String> record) {
         System.out.println("-----------------------------");
-        System.out.println("Found email");
+        System.out.println("LOG: " + record.topic());
         System.out.println(record.key());
         System.out.println(record.value());
         System.out.println(record.partition());
         System.out.println(record.offset());
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.out.println("Email sent");
     }
 
 }
